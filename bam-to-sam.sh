67005f0ee3eabde0d1462f36deae02d8be46806b
thaight <haight@uleth.ca>
2022-10-11 13:20:43 -0600

    Update bam-to-sam.sh

--- bam-to-sam.sh
+++ bam-to-sam.sh
@@ -1,18 +1,17 @@
-  GNU nano 4.6                                                                                       bam-to-sam.sh                                                                                                 
 #!/bin/bash
 #SBATCH --job-name=sample_42-Aza-l-1_CIRIquant
 #SBATCH --time=12:00:00
 #SBATCH --mem=4G
 #SBATCH --account=rrg-zovoilis
 
-while getopts o:i: flag
+while getopts l: flag
 do
   	case "${flag}" in
+            l) input=${OPTARG};;
             o) output=${OPTARG};;
-            i) input=${OPTARG};;
         esac
 done
+module load samtools/1.10
 echo "Converting from Bam file: ""$input";
 echo "Converting to Sam file: ""$output";
-module load samtools/1.10
-echo samtools view -h -o "$output" "$input"
+samtools view -h -o "$output" "$input"
